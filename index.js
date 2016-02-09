62a78f18e572ab8aafb2f88da875ff255504853a
Gergely Nemeth <mail@nemethgergely.com>
2016-02-09 09:32:26 +0100

    feat(alpine): adding alpine base images

---
+++ index.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const path = require('path')
+const read = require('fs-readdir-recursive')
+const mkdirp = require('mkdirp')
+const Handlebars = require('handlebars')
+
+const dockerTemplates = read('.').filter(function (filePath) {
+  return filePath.includes('Dockerfile.hbs')
+})
+
+dockerTemplates.forEach((templatePath) => {
+  const dir = path.resolve(templatePath, '../')
+  console.log(dir)
+  const versions = require(`${dir}/versions`)
+  const source = fs.readFileSync(`${dir}/Dockerfile.hbs`, 'utf-8')
+  const template = Handlebars.compile(source)
+
+  versions.forEach((version) => {
+    const dockerFileLocation = path.join(dir, version)
+    const dockerFileContent = template({
+      version: version
+    })
+
+    mkdirp.sync(dockerFileLocation)
+
+    fs.writeFileSync(`${dockerFileLocation}/Dockerfile`, dockerFileContent)
+  })
+})
